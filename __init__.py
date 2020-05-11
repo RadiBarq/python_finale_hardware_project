f25a319d78151f16d4abaf78f6e7aee8e1656771
RadiBarq <grayllow@gmail.com>
2020-05-11 20:29:23 +0000

    before the destruction

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from camera import Camera
 from ultrasonic import UltraSonic
 from temprature import Temprature
 from moisture import Moisture
+from pump import Pump
 from ldr import LDR
-
+from network_model import NetworkModel
 
--- main_crone.py
+++ main_crone.py
@@ -3,31 +3,78 @@ from ultrasonic import UltraSonic
 from temprature import Temprature
 from moisture import Moisture
 from ldr import LDR
+from pump import Pump
+from network_model import NetworkModel
 import time
+from dateutil import parser
 
-camera = Camera()
-camera.takePicture()
-ultraSonic = UltraSonic()
-temprature = Temprature()
-moisture = Moisture()
-ldr = LDR()
+class Main:
+	def __init__(self):
+		# Initialization
+		self.camera = Camera()
+		self.ultraSonic = UltraSonic()
+		self.temprature = Temprature()	
+		self.moisture = Moisture()
+		self.pump = Pump()	
+		self.ldr = LDR()
+		self.jobs = []
+		self.filteredJobs = []
 
-## Distance
-distance = ultraSonic.getDistance()
-print('distance: {} cm '.format(distance))
-time.sleep(1)
+	def start(self):
+		self.pump.start()
+		self.getJobs()
+		self.filterJobs()
+		self.runJobs()
+	
+	def getJobs(self):	
+		self.jobs = NetworkModel.getAllJobs().json()
+							
+	def filterJobs(self):
+		for job in self.jobs:
+			current_time = parser.parse(job["current_time"])
+			when_to_execute = job["when_to_execute"]
+			last_executed = parser.parse(job["last_executed"])
+			if ((last_executed + datetime.timedelta(minutes = when_to_execute)) >= current_time):
+				self.filteredJobs.append(job)
+			
+			
 
-## Temp result
-tempResult = temprature.readTempratureAndHumadity()
-time.sleep(1)
+	def runJobs(self):
+		for job in self.filteredJobs:
+			jobName = job["name"]
+			if (jobName == "WaterLavel"):
+				## Distance
+				distance = self.ultraSonic.getDistance()
+				print('distance: {} cm '.format(distance))
+				time.sleep(1)
+
+			if (jobName == "Temprature"):
+				## Temp result
+				tempResult = self.temprature.readTempratureAndHumadity()
+				time.sleep(1)
+
+			if (jobName == "Moisture"):
+				## Moisture percentage
+				moisturePercentage = self.moisture.getMoisture()
+				print("moisture is {:>5.3f}%".format(moisturePercentage))
+				time.sleep(1)
+
+			if (jobName == "Brightness"):
+				##LDR 
+				ldrPercentage = self.ldr.readLDR()
+				time.sleep(1)
+			
+			if(jobName == "Camera"):
+				self.camera.takePicture()
 
 
-## Moisture percentage
-moisturePercentage = moisture.getMoisture()
-print("moisture is {:>5.3f}%".format(moisturePercentage))
-time.sleep(1)
+# Start of program
+main = Main()
+main.start()
 
-##LDR 
-ldrPercentage = ldr.readLDR()
-time.sleep(1)
+
+
+
+
+
 
